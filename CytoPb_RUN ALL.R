09288600ca1bbdf8c4874e764eb2f0ce368bae13
Paul Barber <p.barber@ucl.ac.uk>
2023-03-15 15:55:30 +0000

    Comments

--- CytoPb_RUN ALL.R
+++ CytoPb_RUN ALL.R
@@ -1,12 +1,17 @@
 # CytoPb run all script
 
+# Specify or select a working folder
 #working_folder <- "D:/images/IMC/data1"
 working_folder <- choose.dir(caption = "Select data folder")
 
+# Optionally run an import script
 #source("CytoPb_0 Import CODEX tif data.R")
 source("CytoPb_0 Import IMC txt data.R")
-#image_scale_umperpixel = 0.37744  # um per pixel
 
+# Specify options
+#image_scale_umperpixel = 0.37744  # um per pixel, default is 1
+
+# Choose which processing to do
 source("CytoPb_1 Determine Channel Ranges.R")
 source("CytoPb_2 Channel Probabilities.R")
 source("CytoPb_3 Cell Type Probabilities.R")
