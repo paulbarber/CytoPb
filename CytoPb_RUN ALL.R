5c0ec836d09f40c78530558568dec553ae9b1f5a
Paul Barber <p.barber@ucl.ac.uk>
2023-03-22 09:50:50 +0000

    Options.

--- CytoPb_RUN ALL.R
+++ CytoPb_RUN ALL.R
@@ -9,9 +9,9 @@ working_folder <- choose.dir(caption = "Select data folder")
 #source("CytoPb_0 Import IMC txt data.R")
 
 # Specify options
-image_scale_umperpixel = 0.75488  # um per pixel, default is 1
-#probability_threshold = 0.5   # Threshold for "high probability", default is 0.5 (in 3)
-use_global_ranges = FALSE   # can use a global range for each channel or individual for each image, default is FALSE
+#image_scale_umperpixel = 0.75488  # um per pixel, default is 1
+use_global_ranges = TRUE   # can use a global range for each channel or individual for each image, default is FALSE (in 2)
+probability_threshold = 0.1   # Threshold for "high probability", default is 0.5 (in 3)
 
 # Choose which processing to do
 source("CytoPb_1 Determine Channel Ranges.R")
