f421ca7a5265cbf6eb18869cc2558bb4c4c3f6b7
Paul Barber <p.barber@ucl.ac.uk>
2023-03-24 15:31:21 +0000

    Placeholder for new script idea.

---
+++ CytoPb_5 Cell Neighbourhoods.R
@@ -0,0 +1,7 @@
+
+# This file is a placeholder for an intended script that will divide the images into grid tiles and umap cluster them into
+# region types based on the cell type totals (probability density).
+# Cell communities could follow from this based on which region types occur together etc.
+
+
+
